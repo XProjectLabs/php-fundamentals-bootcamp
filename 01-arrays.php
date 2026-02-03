96c8264ae6fbf6ab36192988ecddc1416df2b19c
XProjectLabs <hamza.annane.official@outlook.com>
2026-02-03 13:43:23 -0500

    Working with Associative Arrays

--- 01-arrays.php
+++ 01-arrays.php
@@ -1,16 +1,34 @@
 <?php
 
-//indexed Array
-$color = ['red','blue','yellow'];
-var_dump($color);
+/*
 
-//Associative Array
-$user = [
-    "name" => "php",
-    "Version" => 8
+    Challenge 2: Working with Associative Arrays
+
+    Project Description:
+    You will learn how to work with associative arrays in PHP. These arrays allow you to associate keys with values, instead of using numeric indices.
+
+    To Do:
+
+    Create an associative array $person with the keys name, firstName, age, and city.
+
+    Add values for each corresponding key.
+
+    Add a new key email to this array.
+
+    Use a foreach loop to display all the values in the array.
+*/
+
+
+$person = [
+    'name' => 'youcode',
+    'age'  => 8,
+    'city' => 'Youssofia',
+    'email' => 'admin@youcode.ma'
 ];
 
-var_dump($user);
+foreach ($person as $person => $value)
+    {
+        echo $person . " : ". $value . PHP_EOL;
+    }
 
-echo count($color);
 
